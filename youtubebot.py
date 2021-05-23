4d42c48147037be5784740c3e535cc605c1f81fe
Ewerton J. Silva <76182422+Ewerton-Jose@users.noreply.github.com>
2021-05-23 18:38:15 -0300

    inpunts adicionados
    * input para os segundos de tempo
    * input url
    * e todos com tratamentos de erros

--- youtubebot.py
+++ youtubebot.py
@@ -6,11 +6,30 @@ cwd = os.getcwd()
 xoli = f'{cwd}'.replace(' \ '.strip(), ('/') )
 xoli = xoli + '/geckodriver.exe'
 driver = webdriver.Firefox(executable_path=xoli)
-driver.get("https://www.youtube.com/watch?v=AWzLP25pm5Q")
-sleep(2)
+
+while True:
+    try:
+        tmp = int(input("Digite os segundos do vídeo: ")) + 2
+    except:
+        print("\033[31mERRO! Numero inválido\033[m")
+    else:
+        break
+
+
+while True:
+    try:
+        url = str(input("Escreva a URL: "))
+        driver.get(f"{url}")
+    except:
+        print("\033[31mError! Tem certeza que eessa é a URL?\033[m")
+    else:
+        driver.get(f"{url}")
+
+
+sleep(10)
 reproduzir = driver.find_element_by_xpath('//Button[@title="Reproduzir (k)"]')
 reproduzir.click()
 while True:
-    sleep(12)
+    sleep(tmp)
     reiniciar = driver.find_element_by_xpath('//Button[@title="Reiniciar"]')
     reproduzir.click()
